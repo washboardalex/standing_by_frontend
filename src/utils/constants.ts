6e8878a0085894fea448a4aaf10f441eb03f3328
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-11-15 20:39:53 +1000

    updated admin url for staging app

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ import { AppHeaders } from "./types";
 
 export const covidApiUrl : string = 'https://api.covid19api.com';
 
-export const adminUrl : string = 'http://10.0.2.2:3001';
+export const adminUrl : string = 'https://covid-19-alerts-admin-staging.com.au';
 
 export const headers : AppHeaders = { 
     'Content-Type': 'application/json',
