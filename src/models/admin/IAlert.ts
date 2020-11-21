e831c82cef39a04a2bb3c980801e6ad41d87d5df
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-11-21 19:54:47 +1000

    updated new alert page to allow for more option

--- src/models/admin/IAlert.ts
+++ src/models/admin/IAlert.ts
@@ -1,10 +1,10 @@
 export type AlertCondition = 'greaterThan' | 'lessThan';
-export type TypeCondition = 'newDeaths' | 'newConfirmed';
+export type AlertType = 'newDeaths' | 'newConfirmed';
 
 export interface IAlert {
     country: string,
-    type: TypeCondition,
-    condition: AlertCondition
-    value: number
-    id?: number 
+    type: AlertType,
+    condition: AlertCondition,
+    value: number,
+    id?: number
 }
