3eb699b09a20676676bdd15247508a9e2f76e605
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-11-27 08:00:56 +1000

    firebase validation occurs in active alerts tab now (first view when you open the app)

--- src/screens/active-alerts.component.tsx
+++ src/screens/active-alerts.component.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { fArgReturn, fEmptyReturn } from '../utils/types';
+import { createStructuredSelector } from 'reselect';
+import { Dispatch, AnyAction } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { AppState } from '../redux/root-reducer';
+import { selectFirebaseToken, selectDeviceId } from '../redux/firebase/firebase.selectors';
+import { getFirebaseToken, sendFirebaseTokentoAdminServer } from '../redux/firebase/firebase.actions';
+
+interface IReduxStateProps {
+    firebaseCloudMessageToken: null | string,
+    deviceId: string
+}
+
+interface IDispatchProps {
+    getFirebaseToken: fEmptyReturn,
+    sendFirebaseTokentoAdminServer: fArgReturn
+}
+
+type ActiveAlertsProps = IReduxStateProps & IDispatchProps;
+
+class ActiveAlerts extends React.Component<ActiveAlertsProps> {
+
+    async componentDidMount() {
+
+        const { firebaseCloudMessageToken, getFirebaseToken, deviceId, sendFirebaseTokentoAdminServer } = this.props;
+
+        if (!firebaseCloudMessageToken) {
+            const token = await getFirebaseToken();
+            if (token) sendFirebaseTokentoAdminServer(token, deviceId);
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    Well here we are. The ActiveAlerts component.
+                </Text>
+            </View>
+        );
+    }
+};
 
 
-const ActiveAlerts : React.FC = () => (
-    <View>
-        <Text>
-            Well here we are. The ActiveAlerts component.
-        </Text>
-    </View>
-);
+const mapStateToProps = createStructuredSelector<AppState, IReduxStateProps>({
+    firebaseCloudMessageToken: selectFirebaseToken,
+    deviceId: selectDeviceId
+});
 
-export default ActiveAlerts;
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, AnyAction> | Dispatch<AnyAction>) => ({
+    getFirebaseToken: () => dispatch<any>(getFirebaseToken()),
+    sendFirebaseTokentoAdminServer: (token : string, deviceId: string) => dispatch<any>(sendFirebaseTokentoAdminServer(token, deviceId))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActiveAlerts);
 
--- src/screens/countries-screen.component.tsx
+++ src/screens/countries-screen.component.tsx
@@ -1,67 +1,17 @@
 import React from 'react';
 import { View, Button } from 'react-native';
-import { createStructuredSelector } from 'reselect';
-import { Dispatch, AnyAction } from 'redux';
-import { ThunkDispatch } from 'redux-thunk';
-import { connect } from 'react-redux';
-import { AppState } from '../redux/root-reducer';
-import { selectFirebaseToken, selectDeviceId } from '../redux/firebase/firebase.selectors';
-import { fArgReturn, fEmptyReturn } from '../utils/types';
-import { getFirebaseToken, sendFirebaseTokentoAdminServer } from '../redux/firebase/firebase.actions';
 
 import CountryList from '../components/country-list.component';
-
-interface IReduxStateProps {
-    firebaseCloudMessageToken: null | string,
-    deviceId: string
-}
-
-interface IDispatchProps {
-    getFirebaseToken: fEmptyReturn,
-    sendFirebaseTokentoAdminServer: fArgReturn
-}
 
 interface IReactNavigationProps {
     navigation: any
 }
 
-type CountriesProps = IReduxStateProps & IDispatchProps & IReactNavigationProps;
+const Countries : React.FC<IReactNavigationProps> = ({navigation}) => (
+    <View style={{ flex: 1 }}>
+        <CountryList />
+    </View>
+);
 
-class Countries extends React.Component<CountriesProps>{
-    async componentDidMount() {
-
-        const { firebaseCloudMessageToken, getFirebaseToken, deviceId, sendFirebaseTokentoAdminServer } = this.props;
-
-        if (!firebaseCloudMessageToken) {
-            const token = await getFirebaseToken();
-            if (token) sendFirebaseTokentoAdminServer(token, deviceId);
-        }
-    }
-
-    render() {
-        const { navigation } = this.props;
-
-        return (
-            <View style={{ flex: 1 }}>
-                <CountryList />
-                <View style={{position: 'absolute', bottom: 0, left: 0, right: 0}}>
-                    <Button title='Add a New Alert' onPress={() => navigation.navigate('AddAlert')} />
-                </View>
-            </View>
-        );
-    }
-} 
-
-
-const mapStateToProps = createStructuredSelector<AppState, IReduxStateProps>({
-    firebaseCloudMessageToken: selectFirebaseToken,
-    deviceId: selectDeviceId
-});
-
-const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, AnyAction> | Dispatch<AnyAction>) => ({
-    getFirebaseToken: () => dispatch<any>(getFirebaseToken()),
-    sendFirebaseTokentoAdminServer: (token : string, deviceId: string) => dispatch<any>(sendFirebaseTokentoAdminServer(token, deviceId))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Countries);
+export default Countries;
 
